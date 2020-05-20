f169c4f723af2f6892680e9d8d685e6ffcf738b5
Stevens Qiu <stevensqiu@gmail.com>
2020-05-20 14:55:50 -0700

    [Parse] implemented parse() methods for ast classes, CONTENT, TABLE, TABLE_ROW, TITLE

--- src/ast/TABLE.java
+++ src/ast/TABLE.java
@@ -6,30 +6,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TABLE extends Node {
-    List<ROW> rows = new ArrayList<>();
+    TITLE title;
+    List<TABLE_ROW> TABLERows = new ArrayList<>();
     @Override
     public void parse() {
-        tokenizer.getAndCheckNext("Table:");
 
-        // Parse BoldRow
-        BOLDROW boldRow = new BOLDROW();
-        boldRow.parse();
-        rows.add(boldRow);
+        // Check if there is a title
+        if (!tokenizer.checkToken("\\[")) {
+            // Parse Title
+            title = new TITLE();
+            title.parse();
+        }
 
         // Parse Rows
-        while (tokenizer.moreTokens() && !tokenizer.checkToken("Table:")){
-            ROW row = new ROW();
-            row.parse();
-            rows.add(row);
+        while (tokenizer.checkToken("\\[")){
+            TABLE_ROW TABLERow = new TABLE_ROW();
+            TABLERow.parse();
+            TABLERows.add(TABLERow);
         }
     }
 
     @Override
     public void evaluate() {
-        writer.println("<table bgcolor=black width=600>");
-        for (ROW r : rows){
-            r.evaluate();
-        }
-        writer.println("</table>");
+        // TODO: implement table
     }
 }
