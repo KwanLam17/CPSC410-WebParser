b170c82475a71f4450ab5a242c32cbd31e52cada
Stevens Qiu <stevensqiu@gmail.com>
2020-05-24 19:09:58 -0700

    format html output

--- src/ast/webParserEvaluateHTML.java
+++ src/ast/webParserEvaluateHTML.java
@@ -16,6 +16,13 @@ public class webParserEvaluateHTML implements webParserVisitor<Void> {
         writer.close();
     }
 
+    private void printIndentedLine(String line, int indent) {
+        for (int i=0; i<indent; i++) {
+            writer.print("\t");
+        }
+        writer.println(line);
+    }
+
     @Override
     public Void visit(ELEMENT b) {
         System.out.println("ELEMENT");
@@ -76,7 +83,7 @@ public class webParserEvaluateHTML implements webParserVisitor<Void> {
     public Void visit(ITEM b) {
         System.out.println("ITEM");
 
-        writer.print(b.itemName);
+        printIndentedLine(b.itemName, 4);
         return null;
     }
 
@@ -94,38 +101,40 @@ public class webParserEvaluateHTML implements webParserVisitor<Void> {
     public Void visit(ROW b) {
         System.out.println("ROW");
 
-        writer.println("\t<tr>");
+        printIndentedLine("<tr>", 2);
 
         // Evaluate Items
         for (ITEM item : b.items) {
-            writer.print("\t\t<td bgcolor=white>");
+            printIndentedLine("<td bgcolor=white>", 3);
             item.accept(this);
-            writer.println("</td>");
+            printIndentedLine("</td>", 3);
         }
 
-        writer.println("\t</tr>");
+        printIndentedLine("</tr>", 2);
         return null;
     }
 
     @Override
     public Void visit(TABLE b) {
         System.out.println("TABLE");
+        writer.println("<div id=\"" + b.hashCode() + "\">");
+
         if (b.title != null) {
             b.title.accept(this);
         }
-//        writer.println("<table bgcolor=black " + "style=\"grid-area: " + placement + "\" >");
-        writer.println("<table bgcolor=black " + "id=\"" + b.hashCode() + "\" >");
+        printIndentedLine("<table bgcolor=black>", 1);
         for (ROW r : b.rows){
             r.accept(this);
         }
-        writer.println("</table>");
+        printIndentedLine("</table>", 1);
+        writer.println("</div>");
         return null;
     }
 
     @Override
     public Void visit(TITLE b) {
         System.out.println("TITLE");
-        writer.println(b.title);
+        writer.println("\t" + b.title);
         return null;
     }
 }
